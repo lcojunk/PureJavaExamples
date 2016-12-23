87570de8f9b25301c80e74ca96add2aee5dbd520
lcojunk <lcojunk@gmail.com>
2016-12-23 12:21:00 +0100

    test changes

--- MyExamples/src/myexamples/LambdaExample.java
+++ MyExamples/src/myexamples/LambdaExample.java
@@ -34,7 +34,7 @@ public class LambdaExample {
     private static String[] names = {"Klaus", "Klara", "John", "James", "Kurt", "Jane"};
 
     public static void main(String[] args) {
-        testJavaSet();
+        testToMap();
 //        findByIdUsage();
 //        example1();
 //        example2();
